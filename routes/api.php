cb979dbde4e363d9e42bfa4c285157ed794b93b9
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2025-05-10 19:04:35 +0700

    d

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1/auth')->group(function(){
-    Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
-    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
-    Route::post('/cek-token', [App\Http\Controllers\Api\AuthController::class, 'cekToken']);
-    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:api');
-    Route::get('/profile', [App\Http\Controllers\Api\AuthController::class, 'profile'])->middleware('auth:api');
-    Route::put('edit/profile', [App\Http\Controllers\Api\AuthController::class, 'editProfile'])->middleware('auth:api');
-    Route::post('/register-non-nasabah', [App\Http\Controllers\Api\AuthController::class, 'registerNonNasabah']);
+    Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+    Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+    Route::post('/cek-token', [App\Http\Controllers\API\AuthController::class, 'cekToken']);
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout'])->middleware('auth:API');
+    Route::get('/profile', [App\Http\Controllers\API\AuthController::class, 'profile'])->middleware('auth:API');
+    Route::put('edit/profile', [App\Http\Controllers\API\AuthController::class, 'editProfile'])->middleware('auth:API');
+    Route::post('/register-non-nasabah', [App\Http\Controllers\API\AuthController::class, 'registerNonNasabah']);
 });
