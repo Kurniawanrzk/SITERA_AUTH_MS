17f762de023227b49a6f7d5d072ee3250fd32ed2
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2025-05-10 21:16:16 +0700

    d

--- app/Http/Controllers/API/SuperadminController.php
+++ app/Http/Controllers/API/SuperadminController.php
@@ -16,12 +16,33 @@ class SuperadminController extends Controller
         $inactiveBSU = User::where('role', 'bsu')->where('status_acc', 'inactive')
         ->get('id');
 
+        $user_ids = $inactiveBSU->pluck('id')->toArray();
+        $response_bsu = $client->request("GET", "http://145.79.10.111:8003/api/v1/bsu/cek-bsu-superadmin", [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' => $request->header('Authorization'),
+            ],
+            'json' => [
+                'user_ids' => $user_ids,
+            ],
+        ]);
+        $data_bsu = json_decode($response_bsu->getBody(), true) ?? [];
+        if (!isset($data_bsu['status'])) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data BSU tidak valid atau tidak ditemukan.',
+            ], 404);
+        }
+
         return response()->json([
             'status' => true,
             'message' => 'Daftar pengguna tidak aktif',
-            'data' => $inactiveBSU,
+            'data' => [
+                'bsu' => $data_bsu['data'],
+            ],
         ]);
-        $inactivePerusahaan = User::where('role', 'perusahaan')->where('status_acc', 'inactive')->get();
+        $inactivePerusahaan = User::where('role', 'perusahaan')->where('status_acc', 'inactive')->get('id');
 
         // Gabungkan hasil
         return response()->json([
